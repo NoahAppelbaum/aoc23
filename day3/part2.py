7d589b70f48acd76b9859a5ab1e6a3e53352313e
Noah Appelbaum <noahappelbaum@gmail.com>
2023-12-05 15:32:58 -0600

    solved 3.2 -- employed regular expressions

--- day3/part2.py
+++ day3/part2.py
@@ -1,86 +1,43 @@
-lines = [line.strip() for line in open("in.txt", "r")]
+import re
 
+lines = ["." + line.strip() + "." for line in open("in.txt", "r")]
+lines.insert(0, '{message:<{width}}'.format(message="", width=len(lines[1])))
+lines.append('{message:<{width}}'.format(message="", width=len(lines[1])))
+
+
+# id star locations, then go through and search numbers around those points
+
+stars = []
+for y in range(0, len(lines)):
+    res = re.finditer("[*]", lines[y])
+    for match in res:
+        stars.append((y, match.start()))
+
+
+# Make iterable of numbers -- separated by line? (y), then contained like (start, end, n)
+
+numbers = []
+for y in range(0, len(lines)):
+    matches = re.finditer("\d+", lines[y])
+    numbers.append([
+        (match.start(), match.end(), int(match.group())) for match in matches
+    ])
+
+
+# find numbers in range of *'s
 sum = 0
+for (y, x) in stars:
 
+    touching = []
+    for row in range(y - 1, y + 2):
+        touching += [
+            n for (start, end, n) in numbers[row] if
+            x - 2 < start < x + 2 or
+            x - 1 < end < x + 2
+        ]
+    if len(touching) == 2:
+        sum += touching[0] * touching[1]
 
-
-def check_gear(line_num, i):
-    r_boundary = 1 if len(lines[0]) - 1 > i + 1 else 0
-    line = lines[line_num]
-
-    nums = []
-
-    if i > 0:
-        if line[i - 1].isdigit():
-            n = line[i - 1]
-            j = i - 2
-            while j > 0 and line[j].isdigit():
-                n = line[j] + n
-                j -= 1
-
-            nums.append(n)
-
-    if i + 1 < len(line):
-        if line[i + 1].isdigit():
-            n = line[i + 1]
-            j = i + 2
-            while j < len(line) and line[j].isdigit():
-                n = n + line[j]
-                j += 1
-
-            nums.append(n)
-
-    if line_num > 0:
-        for idx in range(
-            (i - 1 if i > 0 else i),
-            i + 1 + r_boundary
-        ):
-            if lines[line_num-1][idx].isdigit():
-                n = lines[line_num-1][i + 1]
-                j = i + 2
-                while j < len(lines[line_num-1]) and lines[line_num-1][j].isdigit():
-                    n = n + lines[line_num-1][j]
-                    j += 1
-                j = i - 2
-                while j > 0 and lines[line_num-1][j].isdigit():
-                    n = lines[line_num-1][j] + n
-                    j -= 1
-
-                nums.append(n)
-                break
-
-    if line_num < len(lines) - 1:
-        for idx in range(
-            (i - 1 if i > 0 else i),
-            i + 1 + r_boundary
-        ):
-            if lines[line_num + 1][idx].isdigit():
-                n = lines[line_num + 1][i + 1]
-                j = i + 2
-                while j < len(lines[line_num + 1]) and lines[line_num + 1][j].isdigit():
-                    n = n + lines[line_num + 1][j]
-                    j += 1
-                j = i - 2
-                while j > 0 and lines[line_num + 1][j].isdigit():
-                    n = lines[line_num + 1][j] + n
-                    j -= 1
-
-                nums.append(n)
-                break
-
-    print("nums looking like", nums)
-
-    if len(nums) == 2:
-        return int(nums[0]) * int(nums[1])
-    else:
-        return 0
-
-
-for l in range(0, len(lines)):
-    print("Looking at line", l)
-    line = lines[l]
-    for i in range(0, len(line)):
-        if line[i] == "*":
-            sum += check_gear(l, i)
+    print("the star at", (x, y), "is touching", touching)
 
 print(sum)
